8dc3d7c886a3d01003a2596418718f198ca1a0f0
craig.holmes <craig.holmes@lvs1.com>
2024-01-31 23:34:28 +0000

    style: pre-commit updates

--- test/fixtures/provider.tf
+++ test/fixtures/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1"
   required_providers {
     azurecaf = {
-      source = "aztfmod/azurecaf"
+      source  = "aztfmod/azurecaf"
       version = "~> 1"
     }
   }
